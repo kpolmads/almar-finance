67e25483adc8da5b78e8a07f4c7a1bb32aa428af
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2025-04-20 08:57:20 +0800

    required city field

--- resources/views/pages/customer/add/index.blade.php
+++ resources/views/pages/customer/add/index.blade.php
@@ -183,7 +183,7 @@
                                     <label for="city" class="block text-sm font-medium text-gray-700">City
                                         Address</label>
                                     <select name="city" id="city"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                                     <option>Select</option>
                                     @foreach ($cities as $city)
                                         <option value="{{ $city->id }}">{{ $city->city_town }}</option>
