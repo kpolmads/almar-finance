69240b553fd3ee726afcb87b5f46ba3f7ae7a910
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2025-04-20 08:57:48 +0800

    required brgy field

--- resources/views/pages/customer/add/index.blade.php
+++ resources/views/pages/customer/add/index.blade.php
@@ -167,7 +167,7 @@
                                     <label for="barangay" class="block text-sm font-medium text-gray-700">Barangay
                                         Address</label>
                                     <select name="barangay" id="barangay"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                                     <option>Select</option>
                                     @foreach ($barangays as $barangay)
                                         <option value="{{ $barangay->id }}">{{ $barangay->barangay_name }}</option>
